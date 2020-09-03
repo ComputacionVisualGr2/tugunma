b292a39234a4ee36f6efe0b046edb73da9312d0f
VectorLineal <jnobzaa@unal.edu.co>
2020-09-03 13:18:18 -0500

    fixed issues in nex.config file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+    }
+  },
+  assetPrefix: !debug ? '/tugunma/' : '',
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    return config;
+  }
 }
