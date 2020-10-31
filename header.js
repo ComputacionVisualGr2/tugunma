42a2716ef4cea31f7164dafec5577966197a4f60
Duvan <dcalbarracinv@unal.edu.co>
2020-10-31 13:22:51 -0500

    resolving header problems

--- header.js
+++ header.js
@@ -5,10 +5,4 @@ function parallax_height() {
     $(".sample-section").css({ "margin-top": header_height });
     $(".sample-header").css({ height: header_height - scroll_top });
   }
-  parallax_height();
-  $(window).scroll(function() {
-    parallax_height();
-  });
-  $(window).resize(function() {
-    parallax_height();
-  });
+  
