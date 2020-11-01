623c96a2e512644ee967fc6f9ca93d823c0e83a7
Moisés <mbsuarezg@unal.edu.co>
2020-11-01 04:39:01 -0500

    ilusiones 2d añadidas (falta la descripcion)

---
+++ 2d_illusions/stepping_feet.js
@@ -0,0 +1,36 @@
+var x = 0;
+var xspeed = 1;
+var i = 0
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    background(100)
+}
+
+function draw() {
+
+    background(100)
+    if (x + 100 > width || x < 0) {
+        xspeed = -xspeed;
+    }
+    x = x + xspeed
+    createBars()
+    fill(0, 0, 0)
+    rect(x + 25, 200, 125, 50)
+    fill(255, 255, 255)
+    rect(x, 400, 125, 50)
+    fill(0, 0, 0)
+    rect(x - 25, 600, 125, 50)
+    if (!mouseIsPressed && !keyIsPressed) {
+        createBars()
+    }
+}
+
+function createBars() {
+    var len = 20;
+    for (var i = 0; i < width / len; i++) {
+        fill("white");
+        if (i % 2 == 0)
+            rect(i * len, height, len, -height);
+    }
+}
